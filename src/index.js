9a8dfe89bb7cb731efe50249837d28bf1d84f84a
August Dailey <37079788+AugustDailey@users.noreply.github.com>
2021-01-31 14:58:33 -0600

    Organize (#5)
    * 0.1.1
    
    * Organize repo into more traditional npm repo
    
    * 0.1.2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const parser = require('./utils/parser.js')
 
 exports.parse = function(input) {
-    console.log("This function has not been fully implemented.");
     var result = parser.parse(input);
     return result;
   }
